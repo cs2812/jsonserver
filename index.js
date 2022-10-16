80cd251cd74509c43c50d44f40c24cbba18995bf
cs2812 <cs786786786@gmail.com>
2022-10-16 12:48:38 +0530

    test adding route

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 
+//
 const ENV=require("dotenv")
 ENV.config()
+//
 
 const middlewares = jsonServer.defaults()
 
